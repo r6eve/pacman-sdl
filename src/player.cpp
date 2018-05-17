d167a4cf18f86127b6fbbb5d4d36f1a3d32a2b25
r6eve <r6eve179f@gmail.com>
2018-05-17 16:14:41 +0900

    use inline

--- src/player.cpp
+++ src/player.cpp
@@ -4,70 +4,6 @@
 #include "input_manager.hpp"
 #include "map.hpp"
 
-void Player::init_pos() noexcept {
-  switch (player_type_) {
-    case player_type::p1: {
-      pos_ = {block::size * 9, block::size * 18};
-      block_ = {9, 18};
-      next_block_ = {9, 18};
-      dir_ = 1;
-      anime_count_ = 0;
-      anime_weight_ = 0;
-      return;
-    }
-    case player_type::p2: {
-      pos_ = {block::size * 14, block::size * 18};
-      block_ = {14, 18};
-      next_block_ = {14, 18};
-      dir_ = 3;
-      anime_count_ = 0;
-      anime_weight_ = 0;
-      return;
-    }
-    default:
-      // NOTREACHED
-      return;
-  }
-}
-
-void Player::draw(SDL_Surface *screen, const ImageManager &image_manager,
-                  const game_mode mode) const noexcept {
-  switch (player_type_) {
-    case player_type::p1: {
-      SDL_Surface *p_surface = image_manager.get(image::p1);
-      SDL_Rect src;
-      src.x = block::size * dir_;
-      src.y = block::size * anime_count_;
-      src.w = block::size;
-      src.h = block::size;
-      SDL_Rect dst;
-      dst.x = pos_.x;
-      dst.y = pos_.y;
-      SDL_BlitSurface(p_surface, &src, screen, &dst);
-      return;
-    }
-    case player_type::p2: {
-      if (mode != game_mode::battle) {
-        return;
-      }
-      SDL_Surface *p_surface = image_manager.get(image::p2);
-      SDL_Rect src;
-      src.x = block::size * dir_;
-      src.y = block::size * anime_count_;
-      src.w = block::size;
-      src.h = block::size;
-      SDL_Rect dst;
-      dst.x = pos_.x;
-      dst.y = pos_.y;
-      SDL_BlitSurface(p_surface, &src, screen, &dst);
-      return;
-    }
-    default:
-      // NOTREACHED
-      return;
-  }
-}
-
 void Player::move(const InputManager &input_manager, const Map &map,
                   const game_mode mode) noexcept {
   if ((player_type_ == player_type::p2) && (mode != game_mode::battle)) {
@@ -145,32 +81,4 @@ void Player::move(const InputManager &input_manager, const Map &map,
     next_block_.x = -1;
     pos_.x = block::size * next_block_.x;
   }
-}
-
-Point Player::get_pos() const noexcept { return pos_; }
-
-void Player::set_pos(const Point &p) noexcept { pos_ = p; }
-
-void Player::set_pos(const Point &&p) noexcept { pos_ = p; }
-
-Point Player::get_block() const noexcept { return block_; }
-
-int Player::get_life() const noexcept { return life_; }
-
-void Player::set_life(const int life) noexcept { life_ = life; }
-
-unsigned int Player::get_score() const noexcept { return score_; }
-
-void Player::set_score(const unsigned int score) noexcept { score_ = score; }
-
-bool Player::get_damaged() const noexcept { return damaged_p_; }
-
-void Player::set_damaged(const bool damaged_p) noexcept {
-  damaged_p_ = damaged_p;
-}
-
-unsigned int Player::get_power_mode() const noexcept { return power_mode_; }
-
-void Player::set_power_mode(const unsigned int power_mode) noexcept {
-  power_mode_ = power_mode;
 }
--- src/player.hpp
+++ src/player.hpp
@@ -31,31 +31,99 @@ class Player {
  public:
   Player(const unsigned char player_type) : player_type_(player_type) {}
 
-  void init_pos() noexcept;
+  inline void init_pos() noexcept {
+    switch (player_type_) {
+      case player_type::p1: {
+        pos_ = {block::size * 9, block::size * 18};
+        block_ = {9, 18};
+        next_block_ = {9, 18};
+        dir_ = 1;
+        anime_count_ = 0;
+        anime_weight_ = 0;
+        return;
+      }
+      case player_type::p2: {
+        pos_ = {block::size * 14, block::size * 18};
+        block_ = {14, 18};
+        next_block_ = {14, 18};
+        dir_ = 3;
+        anime_count_ = 0;
+        anime_weight_ = 0;
+        return;
+      }
+      default:
+        // NOTREACHED
+        return;
+    }
+  }
 
-  void draw(SDL_Surface *screen, const ImageManager &image_manager,
-            const game_mode mode) const noexcept;
+  inline void draw(SDL_Surface *screen, const ImageManager &image_manager,
+                   const game_mode mode) const noexcept {
+    switch (player_type_) {
+      case player_type::p1: {
+        SDL_Surface *p_surface = image_manager.get(image::p1);
+        SDL_Rect src;
+        src.x = block::size * dir_;
+        src.y = block::size * anime_count_;
+        src.w = block::size;
+        src.h = block::size;
+        SDL_Rect dst;
+        dst.x = pos_.x;
+        dst.y = pos_.y;
+        SDL_BlitSurface(p_surface, &src, screen, &dst);
+        return;
+      }
+      case player_type::p2: {
+        if (mode != game_mode::battle) {
+          return;
+        }
+        SDL_Surface *p_surface = image_manager.get(image::p2);
+        SDL_Rect src;
+        src.x = block::size * dir_;
+        src.y = block::size * anime_count_;
+        src.w = block::size;
+        src.h = block::size;
+        SDL_Rect dst;
+        dst.x = pos_.x;
+        dst.y = pos_.y;
+        SDL_BlitSurface(p_surface, &src, screen, &dst);
+        return;
+      }
+      default:
+        // NOTREACHED
+        return;
+    }
+  }
 
   void move(const InputManager &input_manager, const Map &map,
             const game_mode mode) noexcept;
 
-  Point get_pos() const noexcept;
-  void set_pos(const Point &p) noexcept;
-  void set_pos(const Point &&p) noexcept;
+  inline Point get_pos() const noexcept { return pos_; }
 
-  Point get_block() const noexcept;
+  inline void set_pos(const Point &p) noexcept { pos_ = p; }
+  inline void set_pos(const Point &&p) noexcept { pos_ = p; }
 
-  int get_life() const noexcept;
-  void set_life(const int life) noexcept;
+  inline Point get_block() const noexcept { return block_; }
 
-  unsigned int get_score() const noexcept;
-  void set_score(const unsigned int score) noexcept;
+  inline int get_life() const noexcept { return life_; }
 
-  bool get_damaged() const noexcept;
-  void set_damaged(const bool damaged_p) noexcept;
+  inline void set_life(const int life) noexcept { life_ = life; }
 
-  unsigned int get_power_mode() const noexcept;
-  void set_power_mode(const unsigned int power_mode) noexcept;
+  inline unsigned int get_score() const noexcept { return score_; }
+
+  inline void set_score(const unsigned int score) noexcept { score_ = score; }
+
+  inline bool get_damaged() const noexcept { return damaged_p_; }
+
+  inline void set_damaged(const bool damaged_p) noexcept {
+    damaged_p_ = damaged_p;
+  }
+
+  inline unsigned int get_power_mode() const noexcept { return power_mode_; }
+
+  inline void set_power_mode(const unsigned int power_mode) noexcept {
+    power_mode_ = power_mode;
+  }
 
   ~Player() {}
 };
