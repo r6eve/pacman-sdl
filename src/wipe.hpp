569f4a9f5c9fce419fb015f289b4eafa439e014f
r6eve <r6eve179f@gmail.com>
2018-05-17 17:21:57 +0900

    use inline

--- src/wipe.hpp
+++ src/wipe.hpp
@@ -2,6 +2,7 @@
 #define WIPE_H
 
 #include <SDL/SDL.h>
+#include "def_global.hpp"
 
 class Wipe {
   enum class wipe_type { in, out };
@@ -16,12 +17,20 @@ class Wipe {
   /**
    * Set wipe-in mode for drawing.
    */
-  void set_wipe_in() noexcept;
+  inline void set_wipe_in() noexcept {
+    wipe_variation_ = wipe_type::in;
+    wipe_count_ = screen::height / 10;
+    wipe_count_reach_ = 0;
+  }
 
   /**
    * Set wipe-out mode for drawing.
    */
-  void set_wipe_out() noexcept;
+  inline void set_wipe_out() noexcept {
+    wipe_variation_ = wipe_type::out;
+    wipe_count_ = 0;
+    wipe_count_reach_ = screen::height / 10;
+  }
 
   /**
    * Start drawing. Before execute this function, call set_wipe_in() or
@@ -30,12 +39,37 @@ class Wipe {
    * `limit_x` is the limit for the x-axis to draw. It's bounded on closed
    * interval [0-screen::width] .
    */
-  void draw(SDL_Surface *screen, const unsigned int limit_x) const noexcept;
+  inline void draw(SDL_Surface *screen, const unsigned int limit_x) const
+      noexcept {
+    SDL_Rect dst = {0, 0, static_cast<Uint16>(limit_x),
+                    static_cast<Uint16>(wipe_count_)};
+    const unsigned int dy = screen::height / 10;
+    for (dst.y = 0; dst.y < screen::height; dst.y += dy) {
+      SDL_FillRect(screen, &dst, 0x00000000);
+    }
+  }
 
   /**
    * Return true if wipe finishes.
    */
-  bool update() noexcept;
+  inline bool update() noexcept {
+    if (wipe_count_ == wipe_count_reach_) {
+      return true;
+    }
+
+    switch (wipe_variation_) {
+      case wipe_type::in:
+        --wipe_count_;
+        break;
+      case wipe_type::out:
+        ++wipe_count_;
+        break;
+      default:
+        // NOTREACHED
+        break;
+    }
+    return false;
+  }
 
   ~Wipe() {}
 };
