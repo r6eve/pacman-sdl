6cd73c4e30a59cba936bcb114f29dad9bc49b77b
r6eve <r6eve179f@gmail.com>
2018-05-17 16:57:56 +0900

    use inline

--- src/mixer_manager.hpp
+++ src/mixer_manager.hpp
@@ -2,33 +2,75 @@
 #define MIXER_MANAGER_H
 
 #include <SDL/SDL_mixer.h>
+#include <iostream>
 #include <memory>
 
 namespace music_type {
+
 enum {
   siren = 0,
   beginning,
   death,
   count,
 };
+
 }  // namespace music_type
 
 class MixerManager {
   Mix_Music *music_[music_type::count];
   std::unique_ptr<Mix_Chunk> se_;
 
-  void load_music(const char *path, const unsigned char music_type);
+  inline void load_music(const char *path, const unsigned char music_type) {
+    music_[music_type] = Mix_LoadMUS(path);
+    if (!music_[music_type]) {
+      throw Mix_GetError();
+    }
+  }
 
-  void load_se(const char *path);
+  inline void load_se(const char *path) {
+    const Mix_Chunk *se = Mix_LoadWAV(path);
+    if (!se) {
+      throw Mix_GetError();
+    }
+    se_ = std::make_unique<Mix_Chunk>(*se);
+  }
 
  public:
-  MixerManager() noexcept;
+  MixerManager() noexcept {
+    if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) !=
+        0) {
+      std::cerr << Mix_GetError() << '\n';
+      exit(EXIT_FAILURE);
+    }
 
-  Mix_Music *get_music(const unsigned char music_type) const noexcept;
+    try {
+      load_music("./data/66376e_Pacman_Siren_Sound_Effect.mp3",
+                 music_type::siren);
+      load_music("./data/pacman_beginning.wav", music_type::beginning);
+      load_music("./data/pacman_death.wav", music_type::death);
+      load_se("./data/pacman_chomp.wav");
+    } catch (const char &e) {
+      std::cerr << "error: " << e << '\n';
+      exit(EXIT_FAILURE);
+    }
+  }
 
-  Mix_Chunk *get_se() const noexcept;
+  inline Mix_Music *get_music(const unsigned char music_type) const noexcept {
+    return music_[music_type];
+  }
 
-  ~MixerManager() noexcept;
+  inline Mix_Chunk *get_se() const noexcept { return se_.get(); }
+
+  ~MixerManager() noexcept {
+    Mix_HaltMusic();
+    Mix_HaltChannel(-1);
+    for (unsigned int i = 0; i < music_type::count; ++i) {
+      Mix_FreeMusic(music_[i]);
+    }
+    Mix_FreeChunk(se_.get());
+    Mix_CloseAudio();
+    atexit(Mix_Quit);
+  }
 };
 
 #endif
