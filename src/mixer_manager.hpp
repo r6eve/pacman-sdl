f3d5eec302165806ab9b137b1e5902e58edbcba2
r6eve <r6eve179f@gmail.com>
2019-03-10 11:54:48 +0900

    Change LICENSE to Boost Software License

--- src/mixer_manager.hpp
+++ src/mixer_manager.hpp
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////
+//                                                              //
+//           Copyright r6eve 2019 -                             //
+//  Distributed under the Boost Software License, Version 1.0.  //
+//     (See accompanying file LICENSE_1_0.txt or copy at        //
+//           https://www.boost.org/LICENSE_1_0.txt)             //
+//                                                              //
+//////////////////////////////////////////////////////////////////
+
 #ifndef MIXER_MANAGER_H
 #define MIXER_MANAGER_H
 
