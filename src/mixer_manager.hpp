ee95809efae141c3a1dce7be5a3963304d4cde39
r6eve <r6eve179f@gmail.com>
2019-03-11 07:34:24 +0900

    Some tweaks

--- src/mixer_manager.hpp
+++ src/mixer_manager.hpp
@@ -1,11 +1,11 @@
-//////////////////////////////////////////////////////////////////
-//                                                              //
-//           Copyright r6eve 2019 -                             //
-//  Distributed under the Boost Software License, Version 1.0.  //
-//     (See accompanying file LICENSE_1_0.txt or copy at        //
-//           https://www.boost.org/LICENSE_1_0.txt)             //
-//                                                              //
-//////////////////////////////////////////////////////////////////
+/****************************************************************/
+/*                                                              */
+/*           Copyright r6eve 2019 -                             */
+/*  Distributed under the Boost Software License, Version 1.0.  */
+/*     (See accompanying file LICENSE_1_0.txt or copy at        */
+/*           https://www.boost.org/LICENSE_1_0.txt)             */
+/*                                                              */
+/****************************************************************/
 
 #pragma once
 
